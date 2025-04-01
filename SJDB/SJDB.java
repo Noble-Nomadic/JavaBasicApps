6a2e2507966c20abea0b4448aab615b534a1388a
Alex <alexedwardjames09@gmail.com>
2025-04-02 09:54:25 +1030

    Create SJDB.java
    Basics setuo

---
+++ SJDB/SJDB.java
@@ -0,0 +1,26 @@
+public class SJDB {
+
+    class DataUnit {
+        int ID;
+        int DataType;
+        int DataValue;
+        
+        DataUnit (int newID, int newDataType, int newDataValue) {
+            ID = newID;
+            DataType = newDataType;
+            DataValue = newDataValue;
+        }
+    }
+    
+    
+    DataUnit EncryptUnit(class DataUnit dataUnit, int keyValue) {
+        
+        encryptedID = dataUnit.ID * keyValue
+        
+        encryptedUnit = DataUnit();
+    }
+    
+    public static void main(String[] args) {
+        
+    }
+}
